0a159d16bbc63eda01a9a01a35f40d2ef5d7bad2
Comet-hub <102557425+Comet-hub@users.noreply.github.com>
2023-11-18 22:16:45 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+// Create a server object
+const server = http.createServer((req, res) => {
+    // Set the response header
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+    // Send the response
+    res.end('Hello, world!');
+});
+
+// Start the server
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+
+
